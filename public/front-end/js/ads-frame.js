ca2ca2ce177be9839d50e6df899b13a02b570f2b
gfl-ngocanh <ngocanh@gutfeelinglab.com>
2020-01-15 17:50:57 +0900

    animation fix

--- public/front-end/js/ads-frame.js
+++ public/front-end/js/ads-frame.js
@@ -151,30 +151,64 @@ $(document).ready(function(){
 			scrollTop: $('.chat-container').prop('scrollHeight')
 		});
 	};
-	$.each(chatMessages, function(index, obj) {
-		chatDelay = chatDelay + 2000;
-		chatDelay2 = chatDelay + obj.delay;
-		chatDelay3 = chatDelay2 + 10;
-		scrollDelay = chatDelay;
-		chatTimeString = " ";
-		msgname = "." + obj.name;
-		msginner = ".messageinner-" + obj.name;
-		spinner = ".sp-" + obj.name;
-		if (obj.showTime == true) {
-			chatTimeString = "<span class='message-time'>" + obj.time + "</span>";
-		}
-		$(".chat-message-list").append("<li class='message-" + obj.align + " " + obj.name + "' hidden><div class='sp-" + obj.name + "'><span class='spinme-" + obj.align + "'><div class='spinner'><div class='bounce1'></div><div class='bounce2'></div><div class='bounce3'></div></div></span></div><div class='messageinner-" + obj.name + "' hidden><span class='message-avatar'>"+ obj.img + "</span><br><div class='message-bubble'><span class='message-text'>" + obj.msg + "</span></div>" + chatTimeString + "</div></li>");
-		$(msgname).delay(chatDelay).fadeIn();
-		$(spinner).delay(chatDelay2).hide(1);
-		$(msginner).delay(chatDelay3).fadeIn();
-		setTimeout(onRowAdded, chatDelay);
-		setTimeout(onRowAdded, chatDelay3);
-		chatDelay = chatDelay3;
-	});
-
 	setTimeout(function(){
 
 		$('.popup').attr('class', 'popup hidden');
 
 	}, 2000);
+
+	var i = 0;
+	var k = 0;
+
+	for (k = 0; k < 20; k++) {
+		for (i = 0; i < chatMessages.length; i++) {
+			chatDelay = chatDelay + 2000;
+			chatDelay2 = chatDelay + chatMessages[i].delay;
+			chatDelay3 = chatDelay2 + 10;
+			scrollDelay = chatDelay;
+			chatTimeString = " ";
+			msgname = "." + chatMessages[i].name;
+			msginner = ".messageinner-" + chatMessages[i].name;
+			spinner = ".sp-" + chatMessages[i].name;
+			if (chatMessages[i].showTime == true) {
+				chatTimeString = "<span class='message-time'>" + chatMessages[i].time + "</span>";
+			}
+			$(".chat-message-list").append("<li class='message-" + chatMessages[i].align + " " + chatMessages[i].name + "' hidden><div class='sp-" + chatMessages[i].name + "'><span class='spinme-" + chatMessages[i].align + "'><div class='spinner'><div class='bounce1'></div><div class='bounce2'></div><div class='bounce3'></div></div></span></div><div class='messageinner-" + chatMessages[i].name + "' hidden><span class='message-avatar'>"+ chatMessages[i].img + "</span><br><div class='message-bubble'><span class='message-text'>" + chatMessages[i].msg + "</span></div>" + chatTimeString + "</div></li>");
+			$(msgname).delay(chatDelay).fadeIn();
+			$(spinner).delay(chatDelay2).hide(1);
+			$(msginner).delay(chatDelay3).fadeIn();
+			setTimeout(onRowAdded, chatDelay);
+			setTimeout(onRowAdded, chatDelay3);
+			chatDelay = chatDelay3;
+			console.log(i);
+		}
+	}
+	// $.each(chatMessages, function(index, obj) {
+
+	// 	if (index != 15) {
+	// 		chatDelay = chatDelay + 2000;
+	// 		chatDelay2 = chatDelay + obj.delay;
+	// 		chatDelay3 = chatDelay2 + 10;
+	// 		scrollDelay = chatDelay;
+	// 		chatTimeString = " ";
+	// 		msgname = "." + obj.name;
+	// 		msginner = ".messageinner-" + obj.name;
+	// 		spinner = ".sp-" + obj.name;
+	// 		if (obj.showTime == true) {
+	// 			chatTimeString = "<span class='message-time'>" + obj.time + "</span>";
+	// 		}
+	// 		$(".chat-message-list").append("<li class='message-" + obj.align + " " + obj.name + "' hidden><div class='sp-" + obj.name + "'><span class='spinme-" + obj.align + "'><div class='spinner'><div class='bounce1'></div><div class='bounce2'></div><div class='bounce3'></div></div></span></div><div class='messageinner-" + obj.name + "' hidden><span class='message-avatar'>"+ obj.img + "</span><br><div class='message-bubble'><span class='message-text'>" + obj.msg + "</span></div>" + chatTimeString + "</div></li>");
+	// 		$(msgname).delay(chatDelay).fadeIn();
+	// 		$(spinner).delay(chatDelay2).hide(1);
+	// 		$(msginner).delay(chatDelay3).fadeIn();
+	// 		setTimeout(onRowAdded, chatDelay);
+	// 		setTimeout(onRowAdded, chatDelay3);
+	// 		chatDelay = chatDelay3;
+	// 	} else {
+	// 	}
+	// });
+
+
+
+	
 });
